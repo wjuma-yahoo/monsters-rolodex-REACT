bf99d3750265230b9c1caad809a2e62017167d82
wjuma19dev <wjuma19dev@yahoo.com>
2022-08-08 02:07:45 -0400

    Adding a search bar to find monsters heroes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      monsters: [],
+      searchString: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        this.setState(
+          () => {
+            return {monsters: users}
+          },
+          () => console.log(users)
+        )
+      })
+  }
+
+  render() {
+
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(this.state.searchString)
+    })
+
+    return (
+      <div style={{ textAlign: 'center' }}>
+
+        <input 
+          type="text" 
+          placeholder="Buscar monster" 
+          onChange={(e) => {
+            let searchString = e.target.value.toLowerCase();
+            
+            this.setState(
+              () => {
+                return { searchString }
+              }
+            )
+          }} 
+        />
+        
+        {
+          filteredMonsters.map(monster => {
+            return <h1 key={monster.id}> { monster.name } </h1>
+          }) 
+        }
+
+      </div>
+    )
+  }
 }
-
-export default App;
