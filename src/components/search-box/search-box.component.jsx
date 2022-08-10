ca11473d6a70a5d4b6f70ffbc7277e6967f16826
wjuma-yahoo <wjuma19dev@yahoo.com>
2022-08-09 21:27:17 -0400

    Agregando estilos Css al componente card

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,10 +1,13 @@
 import { Component } from 'react'
 
+import './search-box.styles.css'
+
 export class SearchBox extends Component {
 
     render() {
         return (
             <input 
+                className={'search-box'}
                 type="text" 
                 placeholder={this.props.placeholder} 
                 onChange={this.props.onChangeHandler} 
